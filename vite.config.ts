b36bb0986c7c8880c651ea624297c247702aa584
shelly_lin <liny7544@gmail.com>
2025-08-03 21:13:12 +0800

    [feat] Add vite.config.ts for Vite configuration with React and path aliasing

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-  },
+  base: '/tmdb-home-accessment/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
